34639c914a13920917f21475af9a978cb9892fc6
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-23 01:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [ name, ... kittens];
 }
-function removeLastkitten() {
+function removeLastKitten() {
   return kittens.slice(0,kittens.length-1);
 }
 function removeFirstKitten() {
